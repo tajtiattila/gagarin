3e0e1ae655fa05d1a578aafdbdf97a8b0de5116c
Attila Tajti <attila.tajti@gmail.com>
2017-02-07 18:13:06 +0100

    add hills

--- js/flappy.js
+++ js/flappy.js
@@ -6,6 +6,7 @@ document.body.appendChild(app.view);
 
 PIXI.loader
   .add("assets/stars.png")
+  .add("assets/hills1.png")
   .load(setup)
 
 function setup() {
@@ -16,7 +17,15 @@ function setup() {
   );
   app.stage.addChild(stars);
 
+  var hills1 = new PIXI.extras.TilingSprite(
+    PIXI.loader.resources["assets/hills1.png"].texture,
+    app.renderer.width,
+    app.renderer.height
+  );
+  app.stage.addChild(hills1);
+
   app.ticker.add(function() {
-    stars.tilePosition.x -= 1;
+    stars.tilePosition.x -= 0.1;
+    hills1.tilePosition.x -= 0.3;
   })
 }
