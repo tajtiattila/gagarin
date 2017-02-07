c2d682a9d856aeacdacdc81e608448456fde31a0
Attila Tajti <attila.tajti@gmail.com>
2017-02-07 18:11:05 +0100

    empty stage

---
+++ js/flappy.js
@@ -0,0 +1,11 @@
+// Renderer létrehozása
+var renderer = PIXI.autoDetectRenderer();
+
+// Renderer nézet hozzáadása a body-hoz
+document.body.appendChild(renderer.view);
+
+// Színtér létrehozása az elemeknek
+var stage = new PIXI.Container();
+
+// Stage kirajzolása a rendererre
+renderer.render(stage);
